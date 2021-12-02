adb44720b2bc3eba3d39c53054439ec635995830
Shaun Kent <shaunkent81@gmail.com>
2021-12-01 21:10:51 -0800

    added waiting page and logic to change pages

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
     app.use(express.static(path.join(__dirname, 'public')));
     app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
+    app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist/'));
 });
 
 app.get('/websocket.js', function(req, res) {
@@ -25,7 +26,7 @@ io.on('connection', (socket) => {
 
     //execute when user submits character
     socket.on('enrollment', game => {
-        console.log(`user: ${socket.id}, ${game.realName} has joined as ${game.charName}: ${game.charDesc}`);
+        console.log(`user: ${socket.id}, ${game.realName} has joined`);
         io.emit('enrollment', `${game.realName} has joined the game.`); //this will go to index.html
         if (playerList.length < 4) {
             charType = "Player"
@@ -35,8 +36,7 @@ io.on('connection', (socket) => {
         playerList.push({
             "id": socket.id,
             "name": game.realName,
-            "charName": game.charName,
-            "charDesc": game.charDesc,
+            "score": 0,
             "userType": charType
         }); 
 
--- public/js/websocket.js
+++ public/js/websocket.js
@@ -1,48 +1,46 @@
-document.onmousemove = function() {
-    playShoe();
-};
+// document.onmousemove = function() {
+//     playShoe();
+// };
 
-function playShoe() {
-    var sample = document.getElementById("shoe");
-    try {
-        sample.play();
-        sample.volume = 0.1;
-    }
-    catch(err) {
-        // do nothing -supress pesky errors!
-    }
-}
+// function playShoe() {
+//     var sample = document.getElementById("shoe");
+//     try {
+//         sample.play();
+//         sample.volume = 0.1;
+//     }
+//     catch(err) {
+//         // do nothing -supress pesky errors!
+//     }
+// }
 
 
 var socket = io();
-var startForm = document.getElementById('startForm');
-var realName = document.getElementById('realName');
-var charName = document.getElementById('charName');
-var charDesc = document.getElementById('charDesc');
-var button = document.getElementById('input');
-var banner = document.getElementsByClassName('banner');
+const startForm = document.getElementById('startForm');
+const realName = document.getElementById('realName');
+const button = document.getElementById('input');
+const banner = document.getElementsByClassName('banner');
+const userCreate = document.getElementById('userCreate');
+const playerCards = document.getElementById('playerCards')
 
 
 //nickname button
 startForm.addEventListener('submit', function(e) {
     e.preventDefault();
-    if (charName.value && realName.value && charDesc.value) {
-        startForm.remove();
-        // banner.remove();
+    if (realName.value) {
+        // userCreate.classList.add('hidden');
         let data = {
             realName: realName.value,
-            charName: charName.value,
-            charDesc: charDesc.value
         };
         socket.emit('enrollment', data);
     }
+    
 });
 
 //recieve messages from yourself after sending them
 socket.on('enrollment', function(game) {
     var item = document.createElement('li');
     item.textContent = game;
-    messages.appendChild(item);
+    playerCards.appendChild(item);
     window.scrollTo(0, document.body.scrollHeight);
 });
 
